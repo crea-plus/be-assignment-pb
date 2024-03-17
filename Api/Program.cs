b50c06bf3845b9821fcad43f2f38677115d7a822
gcret <gregor.cretnik@ineor.si>
2024-03-17 20:03:23 +0100

    Docker support

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,7 @@ using Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Service.DTOs.Configuration;
 using Service.Interfaces;
 using Service.Services;
@@ -23,7 +24,35 @@ builder.Services.AddScoped<IFavouriteService, FavouriteService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	// include xml document
+	string xmlPath = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+	c.IncludeXmlComments(xmlPath);
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer key')",
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	});
+	// enable authorize option
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			Array.Empty<String>()
+		}
+	});
+});
 
 // Configure Bearer authentication
 var tokenValidation = builder.Configuration.GetSection(nameof(TokenValidation)).Get<TokenValidation>();
